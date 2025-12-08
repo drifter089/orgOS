21c0ddfe79302921e492cf0c88fad167da0b4005
akshat mittal <akshatmittal.0007@gmail.com>
2025-12-08 14:36:25 +0200

    fix: remove Prisma extensions to fix tRPC type inference

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,30 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { withOptimize } from "@prisma/extension-optimize";
 
 import { env } from "@/env";
 
-const createPrismaClient = () => {
-  const client = new PrismaClient({
+const createPrismaClient = () =>
+  new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
-  // Conditionally enable Optimize only in development when API key is set
-  const shouldUseOptimize = Boolean(
-    env.OPTIMIZE_API_KEY && env.NODE_ENV === "development",
-  );
-
-  return client.$extends(withAccelerate()).$extends(
-    withOptimize({
-      apiKey: env.OPTIMIZE_API_KEY ?? "",
-      enable: shouldUseOptimize,
-    }),
-  );
-};
-
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
